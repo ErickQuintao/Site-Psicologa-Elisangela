8eb15f8904ea89e8c400200d0ed011db8f647046
Erick Quintao <erickquintao@outlook.com>
2023-05-23 16:23:12 -0300

    corrigir todos os erros design

--- script.js
+++ script.js
@@ -31,3 +31,21 @@ prevButton.addEventListener('click', prevSlider);
 
 hideSlider();
 showSlider();
+
+
+
+document.querySelectorAll('.linksrapidos a').forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    var targetId = this.getAttribute('href');
+    var targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      var targetOffset = targetElement.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: targetOffset,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
