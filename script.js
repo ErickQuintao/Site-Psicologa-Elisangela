5ee45c22c577e3a51f71313725b34f474bb2f6f8
Erick Quintao <erickquintao@outlook.com>
2023-06-10 17:58:46 -0300

    corretado versao desktop carrousel ,nova cor no site

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-const sliderItems = Array.from(document.querySelectorAll('.cont'));
-const prevButton = document.getElementById('prev-button');
-const nextButton = document.getElementById('next-button');
-let currentSlide = 0;
-
-function hideSlider() {
-  sliderItems.forEach(item => {
-    item.classList.remove('on');
-  });
-}
-
-function showSlider() {
-  sliderItems[currentSlide].classList.add('on');
-}
-
-function nextSlider() {
-  hideSlider();
-  currentSlide = (currentSlide + 1) % sliderItems.length;
-  showSlider();
-}
-
-function prevSlider() {
-  hideSlider();
-  currentSlide = (currentSlide - 1 + sliderItems.length) % sliderItems.length;
-  showSlider();
-}
-
-nextButton.addEventListener('click', nextSlider);
-prevButton.addEventListener('click', prevSlider);
-
-hideSlider();
-showSlider();
 
 
 
